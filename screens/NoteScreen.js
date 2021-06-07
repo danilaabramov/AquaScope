675d4933a2f8a65bc8eda127d9685fce857f2065
Your Name <67269761+mrden1235@users.noreply.github.com>
2021-06-07 20:53:31 +0400

    bugNote3

--- screens/NoteScreen.js
+++ screens/NoteScreen.js
@@ -1,44 +1,29 @@
 import React, {useState, useMemo} from 'react';
 import { View, Text, StyleSheet, Keyboard, ScrollView, Platform, KeyboardAvoidingView, TextInput, TouchableOpacity} from 'react-native';
 import { useTheme } from "@react-navigation/native";
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import AsyncStorage from "@react-native-community/async-storage";
 import LottieView from 'lottie-react-native'
 
 export const NoteScreen = ({navigation}) => {
     const {colors} = useTheme();
     const theme = useTheme();
+
     const [task, setTask] = useState();
     const [taskItems, setTaskItems] = useState([]);
-    const [active, setActive] = useState([]);
 
     const handleAddTask = async () => {
         Keyboard.dismiss();
         await AsyncStorage.setItem('Tasks', JSON.stringify([...taskItems, task]))
         setTaskItems([...taskItems, task])
         setTask(null);
-
-        await AsyncStorage.setItem('activeTasks', JSON.stringify([...active, false]))
-        setTaskItems([...active, false])
     }
 
-    const canselTask = async (index) => {
+    const completeTask = async (index) => {
         let itemsCopy = [...taskItems];
         itemsCopy.splice(index, 1);
         setTaskItems(itemsCopy)
         await AsyncStorage.setItem('Tasks', JSON.stringify(itemsCopy))
-
-        itemsCopy = [...active];
-        itemsCopy.splice(index, 1);
-        setTaskItems(itemsCopy)
-        await AsyncStorage.setItem('activeTasks', JSON.stringify(itemsCopy))
-    }
-
-    const completeTask = async (index) => {
-        let itemsCopy = [...active];
-        itemsCopy[index] = !itemsCopy[index]
-        setActive([...itemsCopy])
-        await AsyncStorage.setItem('activeTasks', JSON.stringify(itemsCopy))
     }
 
     useMemo(async () => {
@@ -48,43 +33,29 @@ export const NoteScreen = ({navigation}) => {
         } catch (e) {
             console.log(e)
         }
-
-        try {
-            const myArray = await AsyncStorage.getItem('activeTasks');
-            setActive([...JSON.parse(myArray)]);
-        } catch (e) {
-            console.log(e)
-        }
     }, []).then(r => null)
 
     return (
         <View style={[styles.container, {backgroundColor: colors.background}]}>
-            <View style={{ paddingTop: 60, paddingHorizontal: 20, paddingBottom: 20, flexDirection: "row"}}>
+            <View style={{ paddingTop: 80, paddingHorizontal: 20, paddingBottom: 20, flexDirection: "row"}}>
             <TouchableOpacity style={{height: '100%', width: 50, }} onPress={() => navigation.goBack()} >
                 <MaterialCommunityIcons name="arrow-left" size={35} color={colors.text}/>
             </TouchableOpacity>
             <Text style={[styles.sectionTitle, {color: colors.text}]}>Заметки</Text></View>
-            <ScrollView showsVerticalScrollIndicator={false} 
-                contentContainerStyle={{flexGrow: 1}} keyboardShouldPersistTaps='handled'>
+            <ScrollView
+                contentContainerStyle={{flexGrow: 1}} keyboardShouldPersistTaps='handled' style={{marginBottom: 100}}>
                 <View style={styles.tasksWrapper}>
                     <View style={styles.items}>
                         {
                             taskItems.map((item, index) => {
                                     return (
-                                        <TouchableOpacity key={index}  onPress={() => {completeTask(index)}}>
+                                        <TouchableOpacity key={index}  onPress={() => {}}>
                                             <View style={[styles.item, {backgroundColor: colors.background2}]}>
                                                 <View style={styles.itemLeft}>
-
-                                            <View style={[styles.square, {backgroundColor: theme.dark ? '#305970' : '#B9E3F8'}]}>
-                                {         active[index]  ?
-                                             <MaterialCommunityIcons name="check-bold" size={30} color={colors.text} style={{position: 'absolute', bottom: 1, left: 2, opacity: 1}}/>
-                                          : null
-                                }           
-                                             </View>           
-                                                   
-                                                    <Text style={[styles.itemText, {color: colors.text, textDecorationLine: active[index] ? 'line-through' : 'none'}]}>{item}</Text>
+                                                    <View style={styles.square}/>
+                                                    <Text style={[styles.itemText, {color: colors.text}]}>{item}</Text>
                                                 </View>
-                                                <TouchableOpacity onPress={() => canselTask(index)}>
+                                                <TouchableOpacity onPress={() => completeTask(index)}>
                                                     <MaterialCommunityIcons name="close" size={26} color={colors.text}/>
                                                 </TouchableOpacity>
                                             </View>
@@ -94,9 +65,8 @@ export const NoteScreen = ({navigation}) => {
                         }
                     </View>
                 </View>
-                 <View style={{height: 100, width: '100%'}}/>
             </ScrollView>
-            <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={styles.writeTaskWrapper}>
+            <KeyboardAvoidingView /*behavior={Platform.OS === "ios" ? "padding" : "height"}*/ style={styles.writeTaskWrapper}>
                 <TextInput style={[styles.input, {backgroundColor: colors.background2, color: colors.text}]} placeholder={'Напишите заметку'}
                            value={task} onChangeText={text => setTask(text)} placeholderTextColor={'#666'}/>
                            <TouchableOpacity onPress={() => handleAddTask()}>
@@ -169,12 +139,13 @@ const styles = StyleSheet.create({
     square: {
         width: 24,
         height: 24,
+        backgroundColor: '#55BCF6',
+        opacity: 0.4,
         borderRadius: 5,
         marginRight: 15,
     },
     itemText: {
         maxWidth: '80%',
-        fontSize: 18
     },
     circular: {
         width: 12,
